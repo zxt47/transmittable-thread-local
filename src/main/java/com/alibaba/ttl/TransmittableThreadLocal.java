508332eb0fa5d47db214f8b659b5b8f929291cfc
Yang Fang <snoop.fy@gmail.com>
2019-03-01 10:24:41 +0800

    rename thread local transmit method

--- src/main/java/com/alibaba/ttl/TransmittableThreadLocal.java
+++ src/main/java/com/alibaba/ttl/TransmittableThreadLocal.java
@@ -64,6 +64,9 @@ public class TransmittableThreadLocal<T> extends InheritableThreadLocal<T> {
      * This method merely returns reference of its source thread value, and should be overridden
      * if a different behavior is desired.
      *
+     * @param parentValue parent value to be compute
+     * @return computed value
+     *
      * @since 1.0.0
      */
     protected T copy(T parentValue) {
@@ -127,12 +130,13 @@ public class TransmittableThreadLocal<T> extends InheritableThreadLocal<T> {
     }
 
     /**
-     * Register a ThreadLocal to be transmitted.
+     * Register a ThreadLocal to be transmittable.<br/>
+     * Undo by {@link TransmittableThreadLocal#untransmit(ThreadLocal)}
      *
      * @param threadLocal ThreadLocal need to be transmitted.
      * @return false if already been registered.
      */
-    public static boolean register(ThreadLocal threadLocal) {
+    public static boolean transmit(ThreadLocal threadLocal) {
         if (holder.get().containsKey(threadLocal)) {
             return false;
         } else {
@@ -142,12 +146,12 @@ public class TransmittableThreadLocal<T> extends InheritableThreadLocal<T> {
     }
 
     /**
-     * Unregister ThreadLocal which will not be transmitted.
+     * Unregister ThreadLocal which will not be transmittable.
      *
      * @param threadLocal ThreadLocal not need to be transmitted.
      * @return false if not registered or already been unregistered.
      */
-    public static boolean unregister(ThreadLocal threadLocal) {
+    public static boolean untransmit(ThreadLocal threadLocal) {
         if (holder.get().containsKey(threadLocal)) {
             holder.get().remove(threadLocal);
             return true;
@@ -209,7 +213,7 @@ public class TransmittableThreadLocal<T> extends InheritableThreadLocal<T> {
         } else {
             threadLocal.set(value);
             if (null == value) threadLocalRemove(threadLocal);
-            else register(threadLocal);
+            else transmit(threadLocal);
         }
     }
 
