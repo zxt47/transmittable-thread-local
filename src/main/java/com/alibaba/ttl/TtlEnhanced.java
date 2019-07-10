96b3fed5c63c1c07ef02f36ef792170be014cd64
Jerry Lee <oldratlee@gmail.com>
2019-07-10 21:13:13 +0800

    ! temporary commit: check no use deprecated TtlEnhanced at runtime

--- src/main/java/com/alibaba/ttl/TtlEnhanced.java
+++
@@ -1,9 +0,0 @@
-package com.alibaba.ttl;
-
-/**
- * @see com.alibaba.ttl.spi.TtlAttachment
- * @deprecated Use {@link com.alibaba.ttl.spi.TtlEnhanced} instead.
- */
-@Deprecated
-public interface TtlEnhanced {
-}
